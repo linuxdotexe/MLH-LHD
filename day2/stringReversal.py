4d60e2b036aea4ff01994f420813b994c8b2bcc4
sai nivas mangu <sainonbeat99@gmail.com>
2022-01-11 14:20:19 +0530

    add: reverse a string

---
+++ day2/stringReversal.py
@@ -0,0 +1,8 @@
+# ** Take string input
+string = input("Enter the string to be reversed: ")
+
+# ** Print the original string
+print("Original String:", string)
+
+# ** Print the reversed string
+print("Reversed string:", string[::-1])
